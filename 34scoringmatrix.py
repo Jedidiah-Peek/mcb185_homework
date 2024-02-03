3c52002faf6f37d12eeec35ca283269ff9741a05
Jedidiah-Peek <jed@Jedidiahs-Air-2.attlocal.net>
2024-02-03 12:43:50 -0800

    week 4

--- 34scoringmatrix.py
+++ 34scoringmatrix.py
@@ -2,13 +2,12 @@
 
 sequence = 'ACGT'
 
-column1 = ' '
+print('   ', end='')
 for a in sequence:
-	column1 = column1 + ' ' + a
-print(column1)
+	print(a, end='  ')
 for char in sequence:
-	output = char
+	print()
+	print(char, end=' ')
 	for a in sequence:
-		if a == char: output = output + '+1'
-		else:         output = output + '-1'
-	print(output)
+		if a == char: print('+1', end=' ')
+		else:         print('-1', end=' ')
