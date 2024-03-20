39a9bb39e6daccfa48f49ffd5cd77eedcb46ebb3
Jedidiah-Peek <jed@Jedidiahs-Air-2.attlocal.net>
2024-03-19 23:26:40 -0700

    done

--- 92variants.py
+++ 92variants.py
@@ -9,17 +9,17 @@ arg = parser.parse_args()
 
 
 with gzip.open(arg.vcf, 'rt') as dif:
-	points = []
+	changes = []
 	for a in dif:
 		info = a.split()
-		points.append([info[0], info[1]])
+		changes.append({'chr' : info[0], 'place' : info[1], 'type' : []})
 		
-for a in points:
-	loc = []
-	with gzip.open(arg.gff, 'rt') as locate:
-		for b in locate:
-			places = b.split()
-			if places[0] == a[0] and int(a[1]) > int(places[3]) and int(a[1]) < int(places[4]):
-				if places[2] not in loc:
-					loc.append(places[2])
-		if len(loc) > 0: print(f'{a[0]}\t{a[1]}\t{",".join(loc)}')
+with gzip.open(arg.gff, 'rt') as locate:
+	for a in locate:
+		data = a.split()
+		for point in changes:
+			if point['chr'] == data[0] and int(point['place']) > int(data[3]) and int(point['place']) < int(data[4]):
+				if data[2] not in point['type']: point['type'].append(data[2])
+				
+for a in changes:
+	if len(a['type']) > 0: print(f'{a["chr"]}\t{a["place"]}\t{",".join(a["type"])}')
