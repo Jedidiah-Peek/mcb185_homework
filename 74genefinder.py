47029e9c65e322f4d0af23bca4df30a654121278
Jedidiah-Peek <jed@Jedidiahs-Air-2.attlocal.net>
2024-03-20 12:34:55 -0700

    done

--- 74genefinder.py
+++ 74genefinder.py
@@ -3,24 +3,9 @@ import sys
 import dogma
 
 
-def cds_f(nt, size, num):
-	for frame in range(3):
-		last_stop = 0
-		for a in range(frame, len(nt)-2, 3):
-			if nt[a:a+3] == 'ATG':
-				for b in range(a + 3, len(nt)-2, 3):
-					if nt[b:b+3] in ('TAA', 'TAG', 'TGA'):
-						start = a + 1
-						end = b + 3
-						if start > last_stop and end - start >= size:
-							print(f'E. coli\t74genefinder.py\tCDS\t{start}\t{end}\t.\t+\t0\tpro-{num}')
-							num += 1
-						last_stop = end
-						break
-	return num
-
-def cds_r(nt, size, num):
+def find_cds(nt, size, both):
 	l = len(nt)
+	out = []
 	for frame in range(3):
 		last_stop = 0
 		for a in range(frame, len(nt)-2, 3):
@@ -30,14 +15,26 @@ def cds_r(nt, size, num):
 						start = a + 1
 						end = b + 3
 						if start > last_stop and end - start >= size:
-							print(f'E. coli\t74genefinder.py\tCDS\t{l - end}\t{l - start}\t.\t-\t0\tpro-{num}')
-							num += 1
+							out.append({'start': start, 'end' : end, 'direction' : '+'})
 						last_stop = end
 						break
-	return num
-						
+		if both:
+			last_stop = 0
+			op_nt = dogma.revcomp(nt)
+			for a in range(frame, len(nt)-2, 3):
+				if nt[a:a+3] == 'ATG':
+					for b in range(a + 3, len(nt)-2, 3):
+						if nt[b:b+3] in ('TAA', 'TAG', 'TGA'):
+							start = a + 1
+							end = b + 3
+							if start > last_stop and end - start >= size:
+								out.append({'start': l - end, 'end' : l - start, 'direction' : '-'})
+							last_stop = end
+							break
+	return out
+
 
 for defline, seq in mcb185.read_fasta(sys.argv[1]):
-	CDS = 0
-	CDS = cds_f(seq, int(sys.argv[2]), CDS)
-	CDS = cds_r(dogma.revcomp(seq), int(sys.argv[2]), CDS)
+	CDS = find_cds(seq, int(sys.argv[2]), True)
+	for a, data in enumerate(CDS):
+		print(f'E. coli\t74genefinder.py\tCDS\t{data["start"]}\t{data["end"]}\t.\t{data["direction"]}\t0\tpro-{a}')
