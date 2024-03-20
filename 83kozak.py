0494cd8124f3701a1e150fc7288bbbf8ba06e872
Jedidiah-Peek <jed@Jedidiahs-Air-2.attlocal.net>
2024-03-20 13:29:14 -0700

    done

--- 83kozak.py
+++ 83kozak.py
@@ -1,46 +1,59 @@
 import gzip
 import sys
 import dogma
+import mcb185
 
 
-with gzip.open(sys.argv[1], 'rt') as data:
-	seq = []
-	genes = []
-	is_seq = False
-	for line in data:
-		if is_seq is True:
-			seq.append(''.join(line.split()[1:]))
-		if is_seq is False:
-			data = line.split()
-			if data[0] == 'gene':
-				genes.append(data[1])
-			elif data[0] == 'ORIGIN':
-				is_seq = True
-	seq = ''.join(seq)
-	output = []
-	for a in genes:
-		if 'complement' in a:
-			start = int(a[11:-1].split('..')[1])
-			string = dogma.revcomp(seq[start-5:start+9])
-			for b in range(len(string)):
-				if b == len(output):
-					output.append({'a': 0, 'c': 0, 'g': 0, 't': 0})
-				if string[b] != 'N':
-					output[b][string[b]] += 1
+def get_seq(file):
+	genome = []
+	with gzip.open(file, 'rt') as data:
+		for line in data:
+			if 'ORIGIN' in line:
+				break
+		for line in data:
+			words = line.upper().split()
+			for word in words[1:]:
+				genome.append(word)
+	return ''.join(genome)
+	
+def print_pwm(name, ID, de, pwm):
+	print('AC', name)
+	print('XX')
+	print('ID', ID)
+	print('XX')
+	print('DE', de)
+	print('PO\tA\tC\tG\tT')
+	for i, count in enumerate(pwm):
+		a_count = count['A']
+		c_count = count['C']
+		g_count = count['G']
+		t_count = count['T']
+		print(f'{i+1:<8}{a_count:<8}{c_count:<8}{g_count:<8}{t_count:<8}')
+	print('XX')
+	print('//')
+			
+genome_seq = get_seq(sys.argv[1])
+
+kozaks = []
+for i in range(14):
+	kozaks.append({'A': 0, 'C': 0, 'G': 0, 'T': 0})
+
+with gzip.open(sys.argv[1], 'rt') as lines:
+	for line in lines:
+		data = line.split()
+		if data[0] != 'CDS' or 'join' in data[1]:
+			continue
+
+		if 'complement' in data[1]:
+			place = data[1][data[1].index('('):data[1].index(')')]
+			idx = int(place.split('..')[1])
+			seq = mcb185.anti_seq(genome_seq[idx-5:idx+9])
 		else:
-			start = int(a.split('..')[0])
-			string = seq[start-10:start+4]
-			for b in range(len(string)):
-				if b == len(output):
-					output.append({'a': 0, 'c': 0, 'g': 0, 't': 0})
-				if string[b] != 'N':
-					output[b][string[b]] += 1
-	print('AC IMTSU001')
-	print('XX')
-	print('ID ECKOZ')
-	print('XX')
-	print('DE E. coli Kozak sequence')
-	print(f'PO\tA\tC\tG\tT')
-	for a in range(14):
-		print(f'{a+1}\t{output[a]["a"]:<5}\t{output[a]["c"]:<5}\t{output[a]["g"]:<5}\t{output[a]["t"]:<5}')
-	print('XX')
+			place = data[1]
+			idx = int(place.split('..')[0])
+			seq = genome_seq[idx-10:idx+4]
+
+		for i, nt in enumerate(seq):
+			kozaks[i][nt] += 1
+
+print_pwm('IMTSU001', 'ECKOZ', 'E. coli Kozak sequence', kozaks)
