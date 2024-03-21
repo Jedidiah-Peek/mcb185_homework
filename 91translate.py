62073ba577c0c3ed99e00681722d5a7bf8b73abe
Jedidiah-Peek <jed@Jedidiahs-Air-2.attlocal.net>
2024-03-20 20:23:25 -0700

    done

--- 91translate.py
+++ 91translate.py
@@ -10,16 +10,20 @@ parser.add_argument('-m', '--min', type=int, default=100,
 parser.add_argument('-a', '--anti', action='store_true', help='also examine the anti-parallel strand')
 arg = parser.parse_args()
 
+def print_rows(seq):
+	for a in range(0, len(seq), 60):
+		print(seq[a:a+60])
+
 
 for defline, seq in mcb185.read_fasta(arg.file):
 	trans = dogma.translate(seq[seq.find('ATG'):])
 	if '*' in trans and trans.find('*') + 1 >= arg.min:
 		print(f'>{defline}')
-		print(trans[:trans.find('*')])
+		print_rows(trans[:trans.find('*')])
 	if arg.anti:
 		op = dogma.revcomp(seq)
 		if 'ATG' in op:
 			optrans = dogma.translate(op[op.find('ATG'):])
 			if '*' in optrans and optrans.find('*') + 1 >= arg.min:
 				print(f'>{defline} anti')
-				print(optrans[:optrans.find('*')])
+				print_rows(optrans[:optrans.find('*')])
